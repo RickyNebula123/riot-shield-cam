a8e155e2caea1a5d2480fd45512338e657f4c51a
zero2w <txrx@labs>
2025-02-24 04:00:16 +0000

    added data/ directory support

--- riotCam/src/helpers.py
+++ riotCam/src/helpers.py
@@ -1,4 +1,9 @@
+from os import path
 from datetime import datetime
+
+# Constants
 dt_fmt: str = '%d-%m-%y[%H:%M]'
+save_path: str = path.join(path.abspath('..'), 'data/')
+
 def generate_filename() -> str:
-    return datetime.now().strftime(dt_fmt)
+    return save_path + datetime.now().strftime(dt_fmt)
