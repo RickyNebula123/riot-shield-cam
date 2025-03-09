ef64ffa57ce90f213cf5d480b07b799d86291e6a
zero2w <txrx@labs>
2025-03-09 03:05:36 +0000

    leaned camera.py, working with FfmpegOutput

--- riotCam/src/camera.py
+++ riotCam/src/camera.py
@@ -2,12 +2,17 @@ from libcamera import Transform
 from os import path
 from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder, Quality
+from picamera2.outputs import FfmpegOutput
+
+from pprint import pprint
+
 import RPi.GPIO as GPIO
 
 # Constants
 REC_BUTTON: int = 20
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(REC_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+audio_device_name: str = 'alsa_input.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device-00.analog-mono'
 
 # Crop Regions
 mid_screen:  tuple[int, int, int, int] = (0, 0, 1920, 1080)
@@ -45,14 +50,15 @@ resolution_modes: dict[str, dict] = {
     }
         
 class Camera:
-    def __init__(self, res: str ='low'):
+    def __init__(self, res: str ='low', prev: bool = False):
         self.REC: bool = False # Track recording state
+        self.prev: bool = prev
         self.cam: Picamera2 = Picamera2()
         self.initialize(res)
+        self.encoder = H264Encoder(bitrate=8000000)
     
     def initialize(self, res: str) -> None:
         self.set_resolution(res)
-        self.encoder = H264Encoder()
         
     def set_resolution(self, res: str) -> None:
         if res not in resolution_modes.keys():
@@ -61,12 +67,7 @@ class Camera:
     
     def _set_resolution_(self, video_res: str) -> None:
         res: dict = resolution_modes[video_res]
-        conf: dict = self.cam.create_preview_configuration(
-            transform = Transform(vflip=True),
-            sensor = {
-                'output_size': res['size'],
-                'bit_depth': res['bit_depth']})
-        
+        conf: dict = self.cam.create_video_configuration()
         self.update(res, conf)
     
     def update(self, res: dict, conf: dict):
@@ -76,37 +77,34 @@ class Camera:
             self.cam.stop()
             
         self.cam.configure(conf)
-        
-        # Sequence to apply controls (bug?)
-        self.cam.start(show_preview=True) 
-        self.cam.set_controls({'ScalerCrop': res['crop_limits']})
-        
-        if not output:
-            self.cam.stop()
-        
-        self.resolution = res
-        
+        self.cam.set_controls({'ScalerCrop': res['crop_limits'],
+                               'NoiseReductionMode': 0,
+                               'Sharpness': 0.0})
+
     def record(self, filename: str):
         self.REC = True
-        self.cam.start_recording(self.encoder, filename + '.h264', quality=Quality.MEDIUM)
+        output = FfmpegOutput(filename+'.mp4',
+                              audio=True,
+                              audio_device=audio_device_name)
         
-    def stop(self):
-        if self.REC:
-            self.stop_recording()
-            
-        if self.cam.started:
-            self.cam.stop()
-            
+        self.cam.start_recording(encoder=self.encoder, output=output)
+    
+    def simple_record(self, filename):
+        self.REC = True
+        self.cam.start_recording(self.encoder, filename + '.h264')
+    
+    def testcord(self, filename: str):
+        self.REC = True
+        output = FfmpegOutput(filename+'.mp4',
+                              audio=True,
+                              audio_device=audio_device_name)
+        self.cam.start_encoder(self.encoder, output)
+    
+    def stopcording(self):
+        self.cam.stop_encoder()
+    
     def stop_recording(self):
         if self.REC:
             self.cam.stop_recording()
             self.REC = False
             
-if __name__ == '__main__':
-    import helpers
-    c = Camera('low')
-    c.cam.start(show_preview=True)
-    c.record(helpers.generate_filename() + '.h264')
-    
-    input('Enter any key to stop recording: >>')
-    c.stop()
